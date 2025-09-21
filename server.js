a6cae83b50fd2f60ba4f6fa552e74e57ce500d68
Suji <atom.sujitha@gmail.com>
2025-09-21 22:53:02 +0530

    Changed the comments

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 // GET
 
 app.get("/getSimple", (req, res) => {
-  res.send(`Hello welcome to nodejs👋`);
+  res.send(`Hello welcome to our application`);
 });
 
 app.get("/getReq", (req, res) => {
